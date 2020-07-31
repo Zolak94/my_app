9fde0f71a36ef9ba5a236e28f51166f56e602dbd
Nemanja <nemanja@bscomputer.com>
2020-07-31 21:33:29 +0200

    added header and footer containing  boostrap 4, made table on Index view

--- views/Index.php
+++ views/Index.php
@@ -1,1 +1,51 @@
-<h1>INDEX PAGE</h1>
+<?php
+    require_once('views/layouts/Header.php');
+?>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header clearfix">
+                    <h2 class="float-left">Users</h2>
+                    <a href="users/create" title="Add User" class="btn btn-success float-right">New User</a>
+                </div>
+                <?php 
+                if (count($users) > 0) {
+                    echo "<table class='table table-bordered table-striped'>";
+                    echo "<thead>";
+                        echo "<tr>";
+                            echo "<th>ID</th>";
+                            echo "<th>E-mail</th>";
+                            echo "<th>First Name</th>";
+                            echo "<th>Last Name</th>";
+                            echo "<th>Action</th>";
+                        echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+                        foreach ($users as $user) {
+                            echo "<tr>";
+                            echo "<td>" . $user['id'] . "</td>";
+                            echo "<td>" . $user['email'] . "</td>";
+                            echo "<td>" . $user['first_name'] . "</td>";
+                            echo "<td>" . $user['last_name'] . "</td>";
+                            echo "<td>";
+                            echo "<a href='users/show?id=". $user['id'] ."' class='btn btn-outline-primary btn-sm' title='View User' data-toggle='tooltip'><i class='fas fa-eye'></i></a>";
+                            echo " <a href='users/edit?id=". $user['id'] ."' class='btn btn-outline-primary btn-sm' title='Update User' data-toggle='tooltip'><i class='fas fa-pen'></i></a>";
+                            echo " <a href='users/delete?id=". $user['id'] ."' class='btn btn-outline-primary btn-sm' title='Delete User' data-toggle='tooltip'><i class='fas fa-trash-alt'></i></a>";
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";                            
+                    echo "</table>";
+                } else {
+                    echo "<p><em>No records were found in database.</em></p>";
+                }
+                ?>
+            </div>
+        </div>        
+    </div>
+    <?php
+        require_once('views/layouts/Footer.php');
+    ?>
+</body>
+</html>
---
+++ views/layouts/Header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   <!-- Icons -->
+    <script src="https://kit.fontawesome.com/0eeb5c74e2.js" crossorigin="anonymous"></script>
+
+    <title>Dashboard</title>
+    <style type="text/css">
+        .container {
+            padding: 2rem 0rem;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+    <style type="text/css">
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
